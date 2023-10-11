4354cb0e27384d667d1de67e6c441114c39f4cad
Riviere Fougy <rivierefougy@gmail.com>
2023-10-11 11:31:57 -0400

    fix: import terser into rollup.config, install tslib, create build via dist dir

---
+++ dist/cjs/index.js
@@ -0,0 +1,2 @@
+"use strict";require("react");
+//# sourceMappingURL=index.js.map
---
+++ dist/esm/types/components/test-component/Test.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TestProps } from './Test.types';
+/**
+ * @description this component is for testing creation/publishing of npm package, and should be deleted once validated.
+ */
+declare const Test: React.FC<TestProps>;
+export default Test;
