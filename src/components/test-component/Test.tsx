65a59aab0e1ea3944701a7b2c9248cc299358263
Riviere Fougy <rivierefougy@gmail.com>
2023-10-11 11:17:01 -0400

    feat: create Test component and types

---
+++ src/components/test-component/Test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TestProps } from './Test.types'
+
+/**
+ * @description this component is for testing creation/publishing of npm package, and should be deleted once validated.
+ */
+const Test: React.FC<TestProps> = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
---
+++ src/components/test-component/Test.types.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface TestProps {
+  text?: string;
+  primary?: boolean;
+  disabled?: boolean;
+  size?: "small" | "medium" | "large";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
