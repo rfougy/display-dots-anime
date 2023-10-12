de5e492ac9e72af653136fbfb19669113458e45f
Riviere Fougy <rivierefougy@gmail.com>
2023-10-12 11:25:56 -0400

    refactor: add config settings to vite.config and package.json for bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/components/index.ts"),
+      name: "Display Dots Anime",
+      fileName: "display-dots-anime",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
 });
