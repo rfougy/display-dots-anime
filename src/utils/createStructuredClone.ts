013ef936d2fd843990b8868cd9fbcf24871f83d5
Riviere Fougy <rivierefougy@gmail.com>
2023-10-15 20:04:00 -0400

    feat: create createStructuredClone util func

---
+++ src/utils/createStructuredClone.ts
@@ -0,0 +1,5 @@
+export function createStructuredClone(objectToClone: any): any {
+  const stringified: string = JSON.stringify(objectToClone);
+  const parsed = JSON.parse(stringified);
+  return parsed;
+}
