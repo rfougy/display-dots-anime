8b2183523142700c6cfb7cbeeb8c013dea14300b
Riviere Fougy <rivierefougy@gmail.com>
2023-10-18 16:07:10 -0400

    fix(useDisplayDots): change method of iterating over displayDotsVals via Object.keys

--- src/hooks/useDisplayDots.ts
+++ src/hooks/useDisplayDots.ts
@@ -3,7 +3,9 @@ import { useDisplayDotsContext } from "src/context/DisplayDotsContext";
 export const useDisplayDots = () => {
   const displayDotsVals = useDisplayDotsContext();
 
-  for (const val of displayDotsVals) {
+  for (const key of Object.keys(displayDotsVals)) {
+    const val = displayDotsVals[key];
+
     if (val === undefined || val === null)
       throw new Error(
         "useDisplayDots must be used within a DisplayDotsProvider",
