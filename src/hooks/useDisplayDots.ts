7af56aad0e489cee06e24c26c014eadc21c3f7ef
Riviere Fougy <rivierefougy@gmail.com>
2023-10-18 16:38:54 -0400

    refactor(useDisplayDots): remove console logs

--- src/hooks/useDisplayDots.ts
+++ src/hooks/useDisplayDots.ts
@@ -1,18 +1,12 @@
 import { useDisplayDotsContext } from "src/context/DisplayDotsContext";
 
-/**
- * @todo function is being called repeatedly during animation (each step of the animation calls this function). Function should only be called on component mount.
- */
 export const useDisplayDots = () => {
   const displayDotsVals = useDisplayDotsContext();
-  console.log("displayDotsVals: ", displayDotsVals);
 
   const keys = Object.keys(displayDotsVals);
 
   keys.map((key) => {
     const val = displayDotsVals[key];
-    console.log("key: ", key);
-    console.log("val: ", val);
 
     if (val === undefined || val === null)
       throw new Error(
